f77bc30db4d4ce5da754d91181460bf693246085
Benjamin Rommel <50652973+BenjaminBjoernRommel@users.noreply.github.com>
2021-10-03 17:51:48 -0400

    - Added Steamworks.NET library for .NET 5

---
+++ Standalone/TestConsoleApp/Program.cs
@@ -0,0 +1,25 @@
+﻿using Steamworks;
+using System;
+
+namespace TestConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var steamId = new CSteamID(123456798);
+
+            // Initialize Steam API.
+            if (!SteamAPI.Init())
+                return;
+
+            SteamUserStats.RequestCurrentStats();
+
+            // Store personal information.
+            var userId = SteamUser.GetSteamID();
+            var userName = SteamFriends.GetPersonaName();
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
